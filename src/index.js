1ffaf75e270074cce3bb3e55cb379167a75deeb2
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-01 21:28:29 +0100

    update setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'assets/scss/styles.scss';
-import App from 'components/App';
+import App from 'js/routes/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router basename="/math-magicians">
+    <Router basename="/bookstore-cms">
       <App />
     </Router>
   </React.StrictMode>,
