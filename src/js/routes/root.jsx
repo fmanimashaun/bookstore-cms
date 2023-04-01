1f292639d28b7716c4962d2b968c31e357c88fca
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-01 23:44:50 +0100

    Setup the browser router for the application

---
+++ src/js/routes/root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pageheader from 'js/components/pageheader';
+
+const Root = () => (
+  <>
+    <Pageheader />
+    <main className="main">
+      <h1>Main contain here</h1>
+    </main>
+  </>
+);
+
+export default Root;
