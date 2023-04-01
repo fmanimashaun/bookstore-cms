1705c8d842d82b0175c0ea212d5639e8e65c5aca
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-01 23:44:04 +0100

    Add the  page header component

---
+++ src/js/components/pageheader.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable dot-notation */
+import React from 'react';
+import { FaUserAlt } from 'react-icons/fa';
+import styles from 'scss/header.module.scss';
+
+const Pageheader = () => (
+  <header className={styles.header}>
+    <div className={styles['nav-wrapper']}>
+      <span className={styles['logo']}>Bookstore CMS</span>
+      <nav className={styles['navbar']}>
+        <ul className={styles['navlinks']}>
+          <li className={styles['navlinks-item']}>Books</li>
+          <li className={styles['navlinksclea-item']}>Categories</li>
+        </ul>
+      </nav>
+    </div>
+    <button type="button" className={styles['profile']}>
+      <FaUserAlt className={styles['profile-img']} />
+    </button>
+  </header>
+);
+
+export default Pageheader;
