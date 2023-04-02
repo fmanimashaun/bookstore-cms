ac794660bd6359ed9b54e0c51250adaa4079531d
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-02 03:14:03 +0100

    Update the pageheader component

--- src/js/components/pageheader.jsx
+++ src/js/components/pageheader.jsx
@@ -1,23 +1,43 @@
 /* eslint-disable dot-notation */
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import { FaUserAlt } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import styles from 'scss/header.module.scss';
 
-const Pageheader = () => (
+const Pageheader = ({ navLinks }) => (
   <header className={styles.header}>
-    <div className={styles['nav-wrapper']}>
-      <span className={styles['logo']}>Bookstore CMS</span>
-      <nav className={styles['navbar']}>
-        <ul className={styles['navlinks']}>
-          <li className={styles['navlinks-item']}>Books</li>
-          <li className={styles['navlinks-item']}>Categories</li>
-        </ul>
-      </nav>
+    <div className={styles['header__wrapper']}>
+      <div className={styles['nav-wrapper']}>
+        <span className={styles['logo']}>Bookstore CMS</span>
+        <nav className={styles['navbar']}>
+          <ul className={styles['navlinks']}>
+            {navLinks.map((link) => (
+              <NavLink
+                key={link.to}
+                to={link.to}
+                className={styles['navlinks-item']}
+              >
+                {link.label}
+              </NavLink>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <button type="button" className={styles['profile']}>
+        <FaUserAlt className={styles['profile-img']} />
+      </button>
     </div>
-    <button type="button" className={styles['profile']}>
-      <FaUserAlt className={styles['profile-img']} />
-    </button>
   </header>
 );
+
+Pageheader.propTypes = {
+  navLinks: PropTypes.arrayOf(
+    PropTypes.shape({
+      to: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default Pageheader;
