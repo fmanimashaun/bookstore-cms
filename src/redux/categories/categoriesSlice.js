31ef92d7a5f739703c2da8be28d05bae36fc8849
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-05 15:29:22 +0100

    Create store and various feactures slices

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  categories: [],
+};
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {
+    checkStatus: (state) => {
+      return {
+        ...state,
+        categories: 'under construction',
+      };
+    }
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
