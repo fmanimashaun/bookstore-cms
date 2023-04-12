7cfe0981725fc2921e2515a4534cda929dc7093a
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-13 00:47:36 +0100

    Update eslint errors

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import 'assets/scss/styles.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from 'routes/App';
-import { Provider } from "react-redux"
-import store from "redux/store"
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
