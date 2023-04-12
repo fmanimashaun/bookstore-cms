60899af4de8b3dfacc08a10ae667435f7df71fce
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-12 22:50:46 +0100

    Update the book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,33 +2,38 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import styles from 'assets/scss/book.module.scss';
+import { useDispatch } from 'react-redux';
+import { removeBook } from 'redux/books/bookSlice';
 
-const Book = ({ book, handleRemoveBook }) => (
-  <div className={styles.book}>
-    <div className={styles['book__details-wrapper']}>
-      <div className={styles['book__details']}>
-        <span className={styles['book__category']}>{book.category}</span>
-        <h3 className={styles['book__title']}>{book.title}</h3>
-        <p className={styles['book__author']}>{book.author}</p>
-      </div>
-      <div className={styles['book__actions']}>
-        <button className={styles['book__btn']} type="button">
-          Comments
-        </button>
-        <button
-          className={styles['book__btn']}
-          type="button"
-          onClick={() => handleRemoveBook(book.item_id)}
-        >
-          Remove
-        </button>
-        <button className={styles['book__btn']} type="button">
-          Edit
-        </button>
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className={styles.book}>
+      <div className={styles['book__details-wrapper']}>
+        <div className={styles['book__details']}>
+          <span className={styles['book__category']}>{book.category}</span>
+          <h3 className={styles['book__title']}>{book.title}</h3>
+          <p className={styles['book__author']}>{book.author}</p>
+        </div>
+        <div className={styles['book__actions']}>
+          <button className={styles['book__btn']} type="button">
+            Comments
+          </button>
+          <button
+            className={styles['book__btn']}
+            type="button"
+            onClick={() => dispatch(removeBook(book.item_id))}
+          >
+            Remove
+          </button>
+          <button className={styles['book__btn']} type="button">
+            Edit
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.shape({
@@ -37,7 +42,6 @@ Book.propTypes = {
     author: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
-  handleRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
