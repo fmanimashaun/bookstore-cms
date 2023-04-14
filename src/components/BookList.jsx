1adf67ab3641ad2e61f106f960fb665aa8b2c584
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-14 13:18:36 +0100

    Implement the fetch book data from api

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from 'assets/scss/booklist.module.scss';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks } from 'redux/books/bookSlice';
 import Book from './Book';
 
 const Books = () => {
   const bookList = useSelector((state) => state.books.bookList);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   return (
     <section className={styles.booklist}>
-      {bookList.map((book) => (
-        <Book key={book.item_id} book={book} />
-      ))}
+      {Object.entries(bookList)
+        .map(([key, value]) => ({ ...value[0], id: key }))
+        .map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
     </section>
   );
 };
--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,26 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API_KEY = 'iCQQc2iC3QCwTkqPO8vQ';
+
+const URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_KEY}/books`;
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await axios.get(URL);
+  if (response.status !== 200) {
+    throw new Error(response.statusText);
+  }
+  return response.data;
+});
 
 const initialState = {
-  bookList: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  bookList: {},
+  status: 'idle',
 };
 
 const bookSlice = createSlice({
@@ -34,9 +29,25 @@ const bookSlice = createSlice({
       const id = action.payload;
       return {
         ...state,
-        bookList: state.bookList.filter((book) => book.item_id !== id),
+        bookList: state.bookList.filter((book) => book.id !== id),
       };
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      .addCase(fetchBooks.fulfilled, (state, action) => ({
+        ...state,
+        status: 'succeeded',
+        bookList: action.payload,
+      }))
+      .addCase(fetchBooks.rejected, (state) => ({
+        ...state,
+        status: 'failed',
+      }));
   },
 });
 
