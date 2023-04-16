79d8036b8af4ff7de11cc52083080188c45c7bbb
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-16 02:10:17 +0100

    Update page styling

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import styles from 'assets/scss/book.module.scss';
+import Styles from 'assets/scss/book.module.scss';
 import { useDispatch } from 'react-redux';
 import { removeBook } from 'redux/books/bookSlice';
+import ProgressBar from './ProgressBar';
+import ProgressDetails from './ProgressDetails';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
   return (
-    <div className={styles.book}>
-      <div className={styles.book__details}>
-        <div className={styles.book__info}>
-          <span className={styles.book__category}>{book.category}</span>
-          <h3 className={styles.book__title}>{book.title}</h3>
-          <p className={styles.book__author}>{book.author}</p>
+    <div className={Styles.book}>
+      <div className={Styles.book__details}>
+        <div className={Styles.book__info}>
+          <span className={Styles.book__category}>{book.category}</span>
+          <h3 className={Styles.book__title}>{book.title}</h3>
+          <p className={Styles.book__author}>{book.author}</p>
+          <div className={Styles.book__actions}>
+            <button className={Styles.book__btn} type="button">
+              Comments
+            </button>
+            <button
+              className={Styles.book__btn}
+              type="button"
+              onClick={() => dispatch(removeBook(book.item_id))}
+            >
+              Remove
+            </button>
+            <button className={Styles.book__btn} type="button">
+              Edit
+            </button>
+          </div>
         </div>
-        <div className={styles.book__actions}>
-          <button className={styles.book__btn} type="button">
-            Comments
-          </button>
-          <button
-            className={styles.book__btn}
-            type="button"
-            onClick={() => dispatch(removeBook(book.item_id))}
-          >
-            Remove
-          </button>
-          <button className={styles.book__btn} type="button">
-            Edit
-          </button>
+        <div className={Styles.book__status}>
+          <div className={Styles.book__stats}>
+            <ProgressBar progressPercentage={64} />
+          </div>
+          <div className={Styles.book__progress}>
+            <ProgressDetails />
+          </div>
         </div>
       </div>
     </div>
--- src/components/pageheader.jsx
+++ src/components/pageheader.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { FaUserAlt } from 'react-icons/fa';
+import { FiMenu } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
 import styles from 'assets/scss/header.module.scss';
 
 const Pageheader = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef]);
+
+  const handleNavLinkClick = () => {
+    setShowMenu(false);
+  };
+
   const navLinks = [
     { to: '/', label: 'Books' },
     { to: '/categories', label: 'Categories' },
@@ -29,8 +51,31 @@ const Pageheader = () => {
           </nav>
         </div>
         <button type="button" className={styles.profile}>
-          <FaUserAlt className={styles.profile__img} />
+          <FaUserAlt className={styles.profile__icon} />
         </button>
+        <button
+          type="button"
+          className={styles.menu}
+          onClick={() => setShowMenu((prev) => !prev)}
+        >
+          <FiMenu className={styles.menu__icon} />
+        </button>
+        {showMenu && (
+          <nav ref={menuRef} className={styles.navbar__mobile}>
+            <ul className={styles.navlinks}>
+              {navLinks.map((link) => (
+                <NavLink
+                  key={link.to}
+                  to={link.to}
+                  className={styles.navlinks__item}
+                  onClick={handleNavLinkClick}
+                >
+                  {link.label}
+                </NavLink>
+              ))}
+            </ul>
+          </nav>
+        )}
       </div>
     </header>
   );
